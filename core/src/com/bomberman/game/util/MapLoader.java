2bcaa4e8d85f2570ffec4ca09fd174ccbc19539a
Paulo Alexandre Piornedo Panucci <pauloapanucci@l2m28.din.uem.br>
2017-09-28 16:02:47 -0300

    map loader

---
+++ core/src/com/bomberman/game/util/MapLoader.java
@@ -0,0 +1,15 @@
+package com.bomberman.game.util;
+
+import com.badlogic.ashley.core.Engine;
+
+public class MapLoader {
+
+    private void loadMap(String filename){
+
+    }
+
+    private void createObstacles(Engine world){
+
+    }
+    
+}
