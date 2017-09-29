01a8ca1d76bd8aa3864d7fe308b5f15d36a449e9
Paulo Alexandre Piornedo Panucci <pauloapanucci@l2m28.din.uem.br>
2017-09-29 10:14:48 -0300

    zé para de usar windos

--- core/src/com/bomberman/game/util/Textures.java
+++ core/src/com/bomberman/game/util/Textures.java
@@ -5,12 +5,17 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Textures {
-    public static Animation playerOneAnimation;
     public static TextureRegion img;
+
+    public static Animation playerOneAnimation;
     public static  Texture playerOneTexture;
     public static  TextureRegion[] playerOneRegions;
+
     public static  Texture obstacleTexture;
     public static Animation obstacleAnimation;
     public static TextureRegion obstacleRegion;
+
+    public static Texture bombTexture;
+    public static  TextureRegion[] bombRegions;
     public static Animation<TextureRegion> bombAnimation;
 }
